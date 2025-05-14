b88c31cca6e2ced94ea167d32158352ead77f823
EvolveTechno <evolvetechnologies25@gmail.com>
2025-05-14 13:11:57 +0530

    Add files via upload

---
+++ src/main/java/com/example/kubeapp/KubeAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.kubeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KubeAppApplication.class, args);
+    }
+}
